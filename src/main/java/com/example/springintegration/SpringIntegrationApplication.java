dafc46ccc028f8033ab8400c90842fa2b5a416ec
annakhuseinova <annakhuseinova@yandex.ru>
2020-11-07 18:47:18 +0300

    Initial commit

---
+++ src/main/java/com/example/springintegration/SpringIntegrationApplication.java
@@ -0,0 +1,64 @@
+package com.example.springintegration;
+
+import com.example.springintegration.service.PrintService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@ImportResource("integration.xml")
+public class SpringIntegrationApplication implements ApplicationRunner {
+
+    private DirectChannel inputChannel;
+    private DirectChannel outputChannel;
+
+    @Autowired
+    @Qualifier("inputChannel")
+    public void setInputChannel(DirectChannel inputChannel) {
+        this.inputChannel = inputChannel;
+    }
+
+    @Autowired
+    @Qualifier("outputChannel")
+    public void setOutputChannel(DirectChannel outputChannel) {
+        this.outputChannel = outputChannel;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        // Not very easy way of creating messages
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", "value");
+        MessageHeaders headers = new MessageHeaders(map);
+        Message<String> message = new GenericMessage<>("Hello world", headers );
+        PrintService printService = new PrintService();
+        printService.print(message);
+        // An easier way to create messages.
+        Message<String> anotherMessage = MessageBuilder.withPayload("Some payload").setHeader("newHeader",
+                "newHeaderValue").build();
+        inputChannel.send(anotherMessage);
+        outputChannel.subscribe(message1 -> System.out.println(message.getPayload()));
+
+        MessagingTemplate template = new MessagingTemplate();
+    }
+}
---
+++ src/main/java/com/example/springintegration/service/PrintService.java
@@ -0,0 +1,20 @@
+package com.example.springintegration.service;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.Map;
+
+public class PrintService {
+
+    public Message<?> print(Message<String> message){
+        MessageHeaders headers = message.getHeaders();
+        for (Map.Entry<String, Object> entry : headers.entrySet()){
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+        System.out.println(message.getPayload());
+        return MessageBuilder.withPayload("New Payload").build();
+    }
+}
